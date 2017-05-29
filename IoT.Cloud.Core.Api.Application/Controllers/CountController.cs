1226add18b7973a80feeb5a05cd51a7dfbdffd9e
Wessel Loth <wessel@loth.io>
2017-05-29 22:46:50 +0200

    Refactor solution to be more structured

---
+++ IoT.Cloud.Core.Api.Application/Controllers/CountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IoT.Cloud.Core.Api.Application.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountController : Controller
+    {
+        private static int _count = 1;
+
+        [HttpGet("[action]")]
+        public JsonResult Add()
+        {
+            return Json(new
+            {
+                message = "Hello World!",
+                count = _count++
+            });
+        }
+
+        [HttpGet("[action]/{number:int}")]
+        public JsonResult Add(int number)
+        {
+            return Json(new
+            {
+                count = (number + 1)
+            });
+        }
+    }
+}
